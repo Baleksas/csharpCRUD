1789e25f0d79891fdd446b0de62c5a8100e29e3e
Baleksas <alexasbag17@gmail.com>
2023-02-25 13:23:37 +0000

    Feat: initial commit

---
+++ CRUD_app/Models/Task.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace CRUD_app.Models
+{
+	public class Task
+	{
+        public int Id
+        {
+            get;
+             set;
+        }
+        public string TaskName
+        {
+            get;
+             set;
+        }
+        public string TaskExplanation { get; set; }
+
+        public Task()
+        {
+
+        }
+
+    }
+}
+
